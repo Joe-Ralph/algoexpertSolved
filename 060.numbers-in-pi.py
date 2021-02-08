23a1a40d5c89c977a4484d5c2786353a0f2c547e
Joe Ralphin <joeral333@gmail.com>
2021-02-08 09:13:26 +0530

    060

---
+++ 060.numbers-in-pi.py
@@ -0,0 +1,19 @@
+inp1 = ['3141','5','31','2','4153','9','42']
+inp2 = '3141532'
+
+global minDepth
+minDepth = float('inf')
+def numbersInPi(arr,string):
+    hashset  = set(arr)
+    def numbersInPiUtil(string,start,depth):
+        global minDepth
+        if start == len(string):
+            minDepth = min(depth,minDepth)
+            return 
+        for i in range(1,len(string)+1):
+            if string[:i] in hashset:
+                numbersInPiUtil(string[i:],i+1,depth+1)
+    numbersInPiUtil(string,1,0)
+    return minDepth
+
+print(numbersInPi(inp1,inp2))
